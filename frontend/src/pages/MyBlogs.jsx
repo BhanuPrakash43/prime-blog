cc0005f2d0e87b6765563d074ee99c3fc0769084
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-24 23:41:02 +0530

    Remove onClick handler from "Write Blog" link in MyBlogs component

--- frontend/src/pages/MyBlogs.jsx
+++ frontend/src/pages/MyBlogs.jsx
@@ -69,7 +69,6 @@ function MyBlogs() {
                     <Link
                         to="/create"
                         className="font-medium px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white transition-all rounded-lg flex items-center space-x-2"
-                        onClick={() => setShowMenu(false)}
                     >
                         ✏️ <span>Write Blog</span>
                     </Link>
